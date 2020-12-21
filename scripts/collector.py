b653d7588eb09842fc796f83a4a5ea4ab20e1ea5
Jason Nucciarone <nucci.programming@gmail.com>
2020-12-21 18:54:44 -0500

    Format license output

--- scripts/collector.py
+++ scripts/collector.py
@@ -119,17 +119,17 @@ def collector(version, license, compression, directory):
 
     elif license:
         click.echo("""collector: A simple script to collect information about your environment.\n
-            Copyright (C) 2020  Jason C. Nucciarone
-            This program is free software: you can redistribute it and/or modify
-            it under the terms of the GNU General Public License as published by
-            the Free Software Foundation, either version 3 of the License, or
-            (at your option) any later version.
-            This program is distributed in the hope that it will be useful,
-            but WITHOUT ANY WARRANTY; without even the implied warranty of
-            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-            GNU General Public License for more details.
-            You should have received a copy of the GNU General Public License
-            along with this program.  If not, see <https://www.gnu.org/licenses/>.""")
+    Copyright (C) 2020  Jason C. Nucciarone
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.""")
 
     else:
         # Change into directory specified by user to create $USER_info
